e0c7cebc60c6dc97ee7ec18350817dde3f87f544
Harshdeep Singh <56130865+coderhersh@users.noreply.github.com>
2023-01-02 19:25:44 +0530

    Add files via upload

---
+++ Trees/Leetcode Problems/BottomView.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector <int> bottomView(Node *root) {
+        vector<int> ans;
+        if (!root) {
+            return ans;    
+        }
+        
+        queue<pair<Node*, int>> Queue;
+        map<int, int> hashmap;
+        Queue.push({root, 0});
+        
+        while (!Queue.empty()) {
+            int n = Queue.size();
+            for (int i = 0; i < n; ++i) {
+                root = Queue.front().first;
+                int hd = Queue.front().second;
+                Queue.pop();
+                
+                hashmap[hd] = root->data;
+                
+                if (root->left) {
+                    Queue.push({root->left, hd - 1});
+                }
+                
+                if (root->right) {
+                    Queue.push({root->right, hd + 1});
+                }
+            }
+        }
+        
+        map<int, int>::iterator it;
+        for (it = hashmap.begin(); it != hashmap.end(); ++it) {
+            ans.push_back(it->second);
+        }
+        
+        return ans;
+    }
+};
